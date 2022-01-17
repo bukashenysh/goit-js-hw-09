f9fcd7523f246cb72304fbed7c3d4e53addc2cb4
Dmytro Radzivil <dimka1897@gmail.com>
2022-01-17 21:19:56 +0200

    1st task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+import '../css/common.css';
 
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+let timerId = null;
+
+refs.stopBtn.disabled = true;
+
+refs.startBtn.addEventListener('click', () => {
+  timerId = setInterval(changingBgColor, 1000);
+  if (timerId !== null) { 
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+  };
+});
+
+refs.stopBtn.addEventListener('click', () => { 
+  clearInterval(timerId);
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+ });
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+function changingBgColor() { 
+  console.log(getRandomHexColor());
+  refs.body.style.backgroundColor = getRandomHexColor();
+};
