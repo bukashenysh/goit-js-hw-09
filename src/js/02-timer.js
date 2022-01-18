15e2f322540dc4cd6ab14b48703a2f640779ae31
Dmytro Radzivil <dimka1897@gmail.com>
2022-01-18 14:15:01 +0200

    2nd task done

---
+++ src/js/02-timer.js
@@ -0,0 +1,90 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import '../css/common.css';
+
+let intervalId = null;
+let selectedDate = null;
+
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+refs.startBtn.disabled = true;
+
+const timer = {
+  start() {
+    intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const timerValue = selectedDate - currentTime;
+
+      updateTimerValue(convertMs(timerValue))
+    }, 1000);
+  },
+  end() { 
+    clearInterval(intervalId);
+    refs.startBtn.disabled = true;
+    window.alert('Please restart this tab to see small magic ones again:)')
+  },
+};
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const choosedTime = selectedDates[0];
+    const choosedTimeMs = choosedTime.getTime();
+    selectedDate = choosedTimeMs;
+    if (choosedTimeMs < Date.now()) {
+      window.alert("Please choose a date in the future")
+    } else {
+      refs.startBtn.disabled = false;
+    }
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+};
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+};
+
+function updateTimerValue({ days, hours, minutes, seconds }) {
+  refs.days.textContent = addLeadingZero(days);
+  refs.hours.textContent = addLeadingZero(hours);
+  refs.minutes.textContent = addLeadingZero(minutes);
+  refs.seconds.textContent = addLeadingZero(seconds);
+}
+
+refs.startBtn.addEventListener('click', timer.start);
+
+document.addEventListener('keydown', evt => {
+  if (evt.code === 'KeyQ') {
+    console.log('You shall not pass!')
+    timer.end();
+  }
+});
